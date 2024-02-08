ba4ad392e04dcfcad96b78526f0d968c1d70b0d0
juansierr4 <117860735+juansierr4@users.noreply.github.com>
2024-02-08 13:09:59 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,44 +1,29 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Dimensions, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, Dimensions, Image } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
 
-//Get the Window's width and height
-const { height, width } = Dimensions.get( 'window' );
+// Get the Window's width and height
+const { height, width } = Dimensions.get('window');
 
-//Sample card data
+// Sample card data
 const cardData = [
-  { id: 1, color: 'lightgrey', images: [{uri:'https://wallpapercave.com/wp/wp2313951.jpg' }, {uri:'https://th.bing.com/th/id/R.8850da8bb324198606655820ce27475f?rik=rnGdS6O52A233g&riu=http%3a%2f%2fcdn01.cdn.justjared.com%2fwp-content%2fuploads%2f2014%2f12%2fowens-shirtless%2fterry-owens-goes-shirtless-in-rio-03.jpg&ehk=889qL0YT6WMYJAdnb3B%2bi8gqAmtXzFrc58YO75EthhQ%3d&risl=&pid=ImgRaw&r=0'}]},
-  { id: 2, color: 'lightblue', images: [{uri:'https://wallpapercave.com/wp/wp5634572.jpg' }, {uri:'https://www.ocregister.com/wp-content/uploads/2017/12/ldn-l-ufc219-khabib-17-1229.jpg?w=526'}]},
-  { id: 3, color: 'beige', images: [{uri:'https://2.bp.blogspot.com/-i9IuglM3Bo8/Ta-alUofK0I/AAAAAAAAAAY/l9CNuj-3Kmc/s1600/awesomeblackguy.jpg' }, {uri:'https://th.bing.com/th/id/OIP.GEa3i7M1EdnES0LO80mhRwAAAA?rs=1&pid=ImgDetMain'}]},
-  { id: 4, color: 'orange', images: [{uri:'https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/rZJIMvhmliwmde8a6/videoblocks-overweight-man-with-glass-of-beer-on-white-background-fat-guy-drinking-alcohol-unhealthy-way-of-life_hwgh6ntgnb_thumbnail-1080_01.png' }, {uri:'https://www.bing.com/images/search?view=detailV2&ccid=ez9%2fgmqd&id=8C9611506C92478411DE3391673863CE90B5223E&thid=OIP.ez9_gmqdzK-ciGhi5npqFgHaEK&mediaurl=https%3a%2f%2fdm0qx8t0i9gc9.cloudfront.net%2fthumbnails%2fvideo%2frZJIMvhmliwmde8a6%2fvideoblocks-fat-man-pours-beer-from-bottle-into-a-beer-glass-obese-man-with-big-belly-isolated-on-white-background-drinking-alcohol-leads-to-obesity_rdb6htl3s_thumbnail-1080_01.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.7b3f7f826a9dccaf9c886862e67a6a16%3frik%3dPiK1kM5jOGeRMw%26pid%3dImgRaw%26r%3d0&exph=1080&expw=1920&q=fat+guy+with+beer&simid=607989081380447703&FORM=IRPRST&ck=982D30EB281D153A7239583412EB7460&selectedIndex=24&itb=0'}]},
-  { id: 5, color: 'pink', images: [{uri:'https://th.bing.com/th/id/R.9e8a235b88f43441604c91fe2b036703?rik=Lq3cqfHbWM1TjA&pid=ImgRaw&r=0'}, {uri:'https://th.bing.com/th/id/R.475fedb211dd600d11693162ecbe8b61?rik=Jj5emxOazfPT1A&riu=http%3a%2f%2fi1.mirror.co.uk%2fincoming%2farticle6774948.ece%2fALTERNATES%2fs615%2fCollin-Clarke4.jpg&ehk=lZcCxmUZkgyh2GQMPugxsEUUzlrqqZg2mGzHLgV1Xxc%3d&risl=&pid=ImgRaw&r=0'}]}
+  { id: 1, color: 'lightgrey', images: [require('./images/TerryCrews1.jpg'), require('./images/TerryCrews2.jpg')] },
+  { id: 2, color: 'lightblue', images: [require('./images/Khabib1.jpg'), require('./images/Khabib2.webp')] },
 ];
 
-//main app component
+// Main app component
 const App = () => {
-  const [currentImages, setCurrentImages] = useState(cardData.reduce((acc, card) => {
-    acc[card.id] = 0;
-    return acc;
-  }, {}));
-  
-  const handleImageTap = (cardID) => {
-    setCurrentImages(prevImages => {
-      const newImages = { ...prevImages };
-      const card = cardData.find(card => card.id === cardID);
-      const imageCount = card.images.length;
-      newImages[cardID] = (newImages[cardID] + 1) % imageCount;
-      return newImages;
-    });
-  };
+  const [currentImageIndices, setCurrentImageIndices] = useState(cardData.reduce((acc, card) => ({
+    ...acc,
+    [card.id]: 0,
+  }), {}));
 
-  const handleSwipeBottom = () => {
-    console.log('Swiped Down!');
-    // Handle actions when swiped left
-  };
-
-  const handleSwipeTop = () => {
-    console.log('Swiped Up!');
-    // Handle actions when swiped right
+  const handleTapCard = (cardIndex) => {
+    const cardId = cardData[cardIndex].id;
+    setCurrentImageIndices(prevIndices => ({
+      ...prevIndices,
+      [cardId]: (prevIndices[cardId] + 1) % cardData[cardIndex].images.length,
+    }));
   };
 
   // Component's rendering
@@ -50,69 +35,62 @@ const App = () => {
         verticalSwipe={true}
         cards={cardData}
         renderCard={(card) => {
-          const currentImageIndex = currentImages[card.id];
-          const currentImage = card.images[currentImageIndex];
-
+          const currentImageIndex = currentImageIndices[card.id];
           return (
-            <TouchableOpacity 
-              key={card.id} 
-              style={{ ...styles.card, backgroundColor: card.color }} 
-              onPress={() => handleImageTap(card.id)}
-            >
-              <Image source={currentImage} style={styles.cardImage}/>
+            <View key={`${card.id}-${currentImageIndex}`} style={{ ...styles.card, backgroundColor: card.color }}>
+              <Image
+                source={card.images[currentImageIndex]}
+                style={styles.cardImage}
+                resizeMode="cover"
+              />
               <Text style={styles.text}>{card.color}</Text>
-            </TouchableOpacity>
+            </View>
           );
         }}
-        onSwipedBottom={handleSwipeBottom}
-        onSwipedTop={handleSwipeTop}
-        backgroundColor='#F5F5F5'
+        onTapCard={(cardIndex) => handleTapCard(cardIndex)}
+        backgroundColor="#F5F5F5"
         cardIndex={0}
-        infinite={true} // for making it loop (1st card will appear after last card is swiped)
+        infinite={true}
         stackSize={3}
-        stackSeparation={height * 0.03}
-        animateCardOpacity
-        animateOverlayLabelsOpacity
+        stackSeparation={15}
         overlayLabels={{
-          bottom: {
-            title: '✖',
+          left: {
+            title: 'NOPE',
             style: {
               label: {
-                backgroundColor: 'red',
-                borderRadius: width * 0.5,
-                height: height * 0.1,
-                width: height * 0.1,
-                textAlign: 'center',
-                color: '#fff',
+                backgroundColor: 'black',
+                borderColor: 'black',
+                color: 'white',
+                borderWidth: 1,
+                borderRadius: 5,
               },
               wrapper: {
                 flexDirection: 'column',
                 alignItems: 'flex-end',
                 justifyContent: 'flex-start',
-                marginTop: height * 0.05,
-                marginLeft: -height * 0.05
-              }
-            }
+                marginTop: 30,
+                marginLeft: -30,
+              },
+            },
           },
-          top: {
-            title: '✔',
+          right: {
+            title: 'FIGHT',
             style: {
               label: {
-                backgroundColor: 'green',
-                borderRadius: width * 0.5,
-                height: height * 0.1,
-                width: height * 0.1,
-                textAlign: 'center',
-                color: '#fff',
+                backgroundColor: 'black',
+                borderColor: 'black',
+                color: 'white',
+                borderWidth: 1,
+                borderRadius: 5,
               },
               wrapper: {
                 flexDirection: 'column',
                 alignItems: 'flex-start',
                 justifyContent: 'flex-start',
-                marginTop: height * 0.05,
-                marginLeft: height * 0.05
-              }
-            }
+                marginTop: 30,
+                marginLeft: 30,
+              },
+            },
           },
         }}
       />
@@ -123,31 +101,30 @@ const App = () => {
 // Styles for the app
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   card: {
     alignSelf: 'center',
-    width: width * 0.95,
-    height: height * 0.99,
+    width: width * 0.9,
+    height: height * 0.75,
     borderRadius: 10,
-    borderWidth: 0.5,
-    borderColor: '#000',
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
     justifyContent: 'center',
     alignItems: 'center',
   },
   text: {
     textAlign: 'center',
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
     color: '#000',
-    backgroundColor: '#fff'
+    marginTop: 10,
   },
   cardImage: {
     width: '100%',
-    height: '80%',
-    resizeMode: 'cover',
+    height: '100%',
+    borderRadius: 10,
   },
 });
 
-// Export app component
 export default App;
