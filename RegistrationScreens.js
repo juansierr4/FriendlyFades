49886445e371656d4f072f2a8168797200fac2d0
juansierr4 <117860735+juansierr4@users.noreply.github.com>
2024-03-18 19:10:34 -0700

    Update RegistrationScreens.js

--- RegistrationScreens.js
+++ RegistrationScreens.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import { styles } from './AppStyles.js';
-//import messaging from '@react-native-firebase/messaging';
+import auth from '@react-native-firebase/auth';
+import messaging from '@react-native-firebase/messaging';
 
 export function PhoneNumberScreen({ navigation }) {
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -14,27 +15,30 @@ export function PhoneNumberScreen({ navigation }) {
     }
   }, [confirmation, navigation]);
 
+  const formatPhoneNumberToE164 = (phoneNumber) => {
+    const cleaned = (`${phoneNumber}`).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{10})$/);
+    return match ? `+1${match[1]}` : null;
+  };
+
   const sendVerificationCode = async () => {
-    try {
-      const fcmToken = await messaging().getToken(); // Assuming this is necessary for your logic
-  
-      const response = await fetch('https://us-central1-squareupapp-2024.cloudfunctions.net/sendVerificationCode', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ phoneNumber, fcmToken }),
-      });
-  
-      const responseData = await response.json();
-      console.log('Verification triggered successfully:', responseData);
-      // Assuming the server responds with the confirmation object or some identifier
-      setConfirmation(responseData.confirmation);
+    const formattedPhoneNumber = formatPhoneNumberToE164(phoneNumber);
+    
+    if (!formattedPhoneNumber){
+      Alert.alert('Invalid Phone Number', 'The phone number entered is not valid. Please enter a 10-digit US phone number. With only numbers');
+      return;
+    }
+    try{
+      const confirmationResult = await auth().signInWithPhoneNumber(formattedPhoneNumber); // Assuming this is necessary for your logic
+      setConfirmation(confirmationResult);
     } catch (error) {
       console.error('Error sending verification code:', error);
-      Alert.alert('Error', 'Failed to send verification code. Please try again.');
+      Alert.alert('Error', error.message);
     }
-  };
+    };
+
+    
+
   const handleSendCodePress = async () => {
     await sendVerificationCode();
     }
@@ -51,7 +55,7 @@ export function PhoneNumberScreen({ navigation }) {
         value={phoneNumber}
         onChangeText={setPhoneNumber}
       />
-      <TouchableOpacity style={styles.button} onPress={navigation.navigate('NameInput')}>
+      <TouchableOpacity style={styles.button} onPress={handleSendCodePress}>
         <Text style={styles.buttonText}>Send Code</Text>
       </TouchableOpacity>
       {confirmation && (
@@ -71,23 +75,16 @@ export function ConfirmationScreen({ navigation, route }) {
   const [code, setCode] = useState('');
 
   const verifyCode = async () => {
-    if (!confirmation) {
-      console.error('Confirmation object is undefined. Handle the error gracefully.');
-      return; // Optionally navigate back or show an error message.
-    }
-
     try {
-      if (code.length !== 6) {
+      if (code.length !==6) {
         Alert.alert('Error', 'Please enter a valid 6-digit code.');
         return;
       }
-
       await confirmation.confirm(code);
-      // Code is correct, navigate to the next screen or do something else
-      navigation.navigate('EmailInputScreen');
-    } catch (error) {
+      navigation.navigate('WelcomeScreen');
+    } catch(error) {
       console.error('Error verifying code:', error);
-      Alert.alert('Error', error.message); // Inform user about the error
+      Alert.alert('Error', error.message);
     }
   };
 
@@ -108,32 +105,18 @@ export function ConfirmationScreen({ navigation, route }) {
   );
 }
 
-export function EmailInputScreen({ navigation }) {
-  const [email, setEmail] = useState('');
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>What's Your Email?</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email Address"
-        keyboardType="email-address"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('WelcomeScreen')}>
-        <Text style={styles.buttonText}>Next</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
   export function WelcomeScreen({ navigation }) {
     return (
       <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.title}>Welcome to Friendly Fades</Text>
-        <Text style={styles.text}>Before you start, here are some house rules:</Text>
-        {/* Insert your app's house rules here */}
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CreateAccount')}>
+        <Text style={styles.title}>Before you start, here are some house rules:</Text>
+        <Text style={styles.text}>Swipe up, Square up: Find your match, then bring your best game.</Text>
+        <Text style={styles.text}>Respect your Rival: Every fade's friendly. Respect on and off the mat</Text>
+        <Text style={styles.text}>Safety First: Sparring's fun with supervision. Only roll with pros watching.</Text>
+        <Text style={styles.text}>Keep it real: Genuine profiles only. Highlight your real skills and experience</Text>
+        <Text style={styles.text}>Manage Your Matches: Your fades, your rules. But always play fair.</Text>
+
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('NameInput')}>
           <Text style={styles.buttonText}>Agree and Continue</Text>
         </TouchableOpacity>
       </ScrollView>
